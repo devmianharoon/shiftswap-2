3a8afdee90b0c43a7e2f4053f68c446aea30c372
devmianharoon <devmianharoon@gmail.com>
2025-05-27 21:05:39 +0500

    first commi t

---
+++ app/(defaults)/page.tsx
@@ -0,0 +1,14 @@
+// import ComponentsDashboardAnalytics from '@/components/dashboard/components-dashboard-sales';
+import ComponentsDashboardAnalytics from '@/components/dashboard/components-dashboard-analytics';
+import { Metadata } from 'next';
+import React from 'react';
+
+export const metadata: Metadata = {
+    title: 'Sales Admin',
+};
+
+const Sales = () => {
+    return <ComponentsDashboardAnalytics />;
+};
+
+export default Sales;
