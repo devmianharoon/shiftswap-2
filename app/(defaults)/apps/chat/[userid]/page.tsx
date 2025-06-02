ee23bffae708bf6b6836205cbd6e5a19c37cbbbc
devmianharoon <devmianharoon@gmail.com>
2025-06-02 13:16:31 +0500

    bug fixes

--- app/(defaults)/apps/chat/[userid]/page.tsx
+++ app/(defaults)/apps/chat/[userid]/page.tsx
@@ -1,12 +1,16 @@
 'use client';
 import ComponentsAppsChat from '@/components/apps/chat/components-apps-chat';
-
 import { useParams } from 'next/navigation';
 import React from 'react';
 
 const Chat = () => {
   const params = useParams();
-  const chatId = params.userid; // or params['id']
+  const chatId = params?.userid as string | undefined;
+
+  if (!chatId) {
+    return <div>Error: No chat ID provided.</div>; // or handle gracefully
+  }
+
   return <ComponentsAppsChat loginUserId={chatId} />;
 };
 
