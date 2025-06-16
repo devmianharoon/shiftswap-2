b136a5fc8198e8d1392bf2658c474cf294dda07b
Syed-Khizar-k <khizarsyed4294@gmail.com>
2025-06-16 19:47:35 +0500

    changes done

--- components/layouts/sidebar.tsx
+++ components/layouts/sidebar.tsx
@@ -187,7 +187,7 @@ const Sidebar = () => {
                                     </Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link href="/dashboard/apps/calendar" className="group">
+                                    <Link href="/dashboard/swap-request" className="group">
                                         <div className="flex items-center">
                                             <ArrowLeftRight className="shrink-0 group-hover:!text-primary" />
                                             <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">{t('Swap Requests')}</span>
