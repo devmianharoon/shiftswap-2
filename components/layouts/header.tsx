88cc86955f9d776607a0f149212a20a12e606a7e
UsamaBinAbdulWahid <usamaabdulwahid14@gmail.com>
2025-06-17 14:26:27 +0500

    profile menu updated

--- components/layouts/header.tsx
+++ components/layouts/header.tsx
@@ -35,6 +35,9 @@ import { usePathname, useRouter } from 'next/navigation';
 import { getTranslation } from '@/i18n';
 
 const Header = () => {
+    const userData = localStorage.getItem('user_data');
+    const parsedUserData = userData ? JSON.parse(userData) : null;
+    console.log(parsedUserData);
     const pathname = usePathname();
     const dispatch = useDispatch();
     const router = useRouter();
@@ -412,19 +415,29 @@ const Header = () => {
                                 offset={[0, 8]}
                                 placement={`${isRtl ? 'bottom-start' : 'bottom-end'}`}
                                 btnClassName="relative group block"
-                                button={<img className="h-9 w-9 rounded-full object-cover saturate-50 group-hover:saturate-100" src="/assets/images/user-profile.jpeg" alt="userProfile" />}
+                                button={
+                                    <img
+                                        className="h-9 w-9 rounded-full object-cover saturate-50 group-hover:saturate-100"
+                                        src={`https://drupal-shift-swap.asdev.tech/sites/default/files/${parsedUserData.logo}`}
+                                        alt="userProfile"
+                                    />
+                                }
                             >
                                 <ul className="w-[230px] !py-0 font-semibold text-dark dark:text-white-dark dark:text-white-light/90">
                                     <li>
                                         <div className="flex items-center px-4 py-4">
-                                            <img className="h-10 w-10 rounded-md object-cover" src="/assets/images/user-profile.jpeg" alt="userProfile" />
+                                            <img
+                                                className="h-10 w-10 rounded-md object-cover"
+                                                src={`https://drupal-shift-swap.asdev.tech/sites/default/files/${parsedUserData.logo}`}
+                                                alt="userProfile"
+                                            />
                                             <div className="truncate ltr:pl-4 rtl:pr-4">
-                                                <h4 className="text-base">
-                                                    John Doe
+                                                <h4 className="text-base capitalize">
+                                                    {parsedUserData.name}
                                                     <span className="rounded bg-success-light px-1 text-xs text-success ltr:ml-2 rtl:ml-2">Pro</span>
                                                 </h4>
                                                 <button type="button" className="text-black/60 hover:text-primary dark:text-dark-light/60 dark:hover:text-white">
-                                                    johndoe@gmail.com
+                                                    {parsedUserData.email}
                                                 </button>
                                             </div>
                                         </div>
