0d4bacb17de3a8c4551175486c768a0ddaea837d
UsamaBinAbdulWahid <usamaabdulwahid14@gmail.com>
2025-06-17 16:11:49 +0500

    another item added

--- components/layouts/sidebar.tsx
+++ components/layouts/sidebar.tsx
@@ -148,6 +148,14 @@ const Sidebar = () => {
                                                     </div>
                                                 </Link>
                                             </li>
+                                            <li className="nav-item">
+                                                <Link href="/dashboard/apps/calendar" className="group">
+                                                    <div className="flex items-center">
+                                                        <CalendarClock className="shrink-0 group-hover:!text-primary" />
+                                                        <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">{t('Shifts')}</span>
+                                                    </div>
+                                                </Link>
+                                            </li>
                                         </>
                                     )}
                                     {parsedUserData.account_type === 'business' && (
