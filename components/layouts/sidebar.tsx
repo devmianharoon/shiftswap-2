8e67183255fadbf85f73162dd14a61c9fda614a0
UsamaBinAbdulWahid <usamaabdulwahid14@gmail.com>
2025-06-18 11:16:20 +0500

    sidebar errors resolved

--- components/layouts/sidebar.tsx
+++ components/layouts/sidebar.tsx
@@ -87,9 +87,7 @@ const Sidebar = () => {
         selector?.classList.add('active');
     };
 
-    const userData = localStorage.getItem('user_data');
-    const parsedUserData = userData ? JSON.parse(userData) : null;
-    console.log(parsedUserData.logo);
+
 
     return (
         <div className={semidark ? 'light' : ''}>
@@ -121,7 +119,6 @@ const Sidebar = () => {
                                     </div>
                                 </Link>
                             </li>
-<<<<<<< HEAD
                             <li className="nav-item">
                                 <Link href="/dashboard/swap-requests" className="group">
                                     <div className="flex items-center">
@@ -130,12 +127,6 @@ const Sidebar = () => {
                                     </div>
                                 </Link>
                             </li>
-=======
-                            {/* {parsedUserData.account_type === 'member' && (
-                                
-                            )} */}
-
->>>>>>> 666e3af3895671cc92718aa5cb0fb5266448ea04
                             <li className="nav-item">
                                 <ul>
                                     {/* <li className="nav-item">
@@ -146,7 +137,6 @@ const Sidebar = () => {
                                             </div>
                                         </Link>
                                     </li> */}
-<<<<<<< HEAD
                                     <li className="nav-item">
                                         <Link href="/dashboard/my-swap-requests" className="group">
                                             <div className="flex items-center">
@@ -171,97 +161,6 @@ const Sidebar = () => {
                                             </div>
                                         </Link>
                                     </li>
-=======
-                                    {parsedUserData.roles?.some((role: string) => ['authenticated'].includes(role)) && (
-                                        <>
-                                            <li className="nav-item">
-                                                <Link href="/dashboard/swap-requests" className="group">
-                                                    <div className="flex items-center">
-                                                        <ArrowLeftRight className="shrink-0 group-hover:!text-primary" />
-                                                        <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">{t('Swap Requests')}</span>
-                                                    </div>
-                                                </Link>
-                                            </li>
-                                            <li className="nav-item">
-                                                <Link href="/dashboard/my-swap-requests" className="group">
-                                                    <div className="flex items-center">
-                                                        <ArrowLeftRight className="shrink-0 group-hover:!text-primary" />
-                                                        <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">{t('My Swap Requests')}</span>
-                                                    </div>
-                                                </Link>
-                                            </li>
-                                            <li className="nav-item">
-                                                <Link href="/dashboard/apps/chat" className="group">
-                                                    <div className="flex items-center">
-                                                        <CalendarClock className="shrink-0 group-hover:!text-primary" />
-                                                        <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">{t('Chats')}</span>
-                                                    </div>
-                                                </Link>
-                                            </li>
-                                        </>
-                                    )}
-                                    {parsedUserData.roles?.some((role: string) => ['business_admin'].includes(role)) && (
-                                        <>
-                                            <li className="nav-item">
-                                                <Link href="/dashboard/manage-swap-requests" className="group">
-                                                    <div className="flex items-center">
-                                                        <ArrowLeftRight className="shrink-0 group-hover:!text-primary" />
-                                                        <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">{t('Manage Swap Requests')}</span>
-                                                    </div>
-                                                </Link>
-                                            </li>
-                                            <li className="nav-item">
-                                                <Link href="/dashboard/apps/calendar" className="group">
-                                                    <div className="flex items-center">
-                                                        <CalendarClock className="shrink-0 group-hover:!text-primary" />
-                                                        <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">{t('Shifts')}</span>
-                                                    </div>
-                                                </Link>
-                                            </li>
-                                            <li className="nav-item">
-                                                <Link href="/dashboard/groups" className="group">
-                                                    <div className="flex items-center">
-                                                        <Layers3 className="shrink-0 group-hover:!text-primary" />
-                                                        <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">{t('Groups')}</span>
-                                                    </div>
-                                                </Link>
-                                                {/* <li className="nav-item">
-                                    <Link href="/dashboard/apps/contacts" className="group">
-                                        <div className="flex items-center">
-                                            <FileBarChart className="shrink-0 group-hover:!text-primary" />
-                                            <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">{t('Reports')}</span>
-                                        </div>
-                                    </Link>
-                                </li> */}
-                                                {/* <li className="nav-item">
-                                    <Link href="/dashboard/apps/contacts" className="group">
-                                        <div className="flex items-center">
-                                            <Settings className="shrink-0 group-hover:!text-primary" />
-                                            <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">{t('Settings')}</span>
-                                        </div>
-                                    </Link>
-                                </li> */}
-
-                                                <li className="nav-item">
-                                                    <Link href="/dashboard/members" className="group">
-                                                        <div className="flex items-center">
-                                                            <UserRound className="shrink-0 group-hover:!text-primary" />
-                                                            <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">{t('Members')}</span>
-                                                        </div>
-                                                    </Link>
-                                                </li>
-                                            </li>
-                                            <li className="nav-item">
-                                                <Link href="/dashboard/apps/chat" className="group">
-                                                    <div className="flex items-center">
-                                                        <CalendarClock className="shrink-0 group-hover:!text-primary" />
-                                                        <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">{t('Chats')}</span>
-                                                    </div>
-                                                </Link>
-                                            </li>
-                                        </>
-                                    )}
->>>>>>> 666e3af3895671cc92718aa5cb0fb5266448ea04
                                 </ul>
                             </li>
 
@@ -285,7 +184,6 @@ const Sidebar = () => {
                                     </div>
                                 </Link>
                             </li> */}
-<<<<<<< HEAD
                             <li className="nav-item">
                                 <Link href="/dashboard/groups" className="group">
                                     <div className="flex items-center">
@@ -319,8 +217,6 @@ const Sidebar = () => {
                                     </Link>
                                 </li>
                             </li>
-=======
->>>>>>> 666e3af3895671cc92718aa5cb0fb5266448ea04
                         </ul>
                     </PerfectScrollbar>
                 </div>
