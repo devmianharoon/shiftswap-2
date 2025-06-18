f77740cd3396d2bb0c69f38851f37ae3387801c9
devmianharoon <devmianharoon@gmail.com>
2025-06-18 15:20:11 +0500

    chat test

--- app/dashboard/(defaults)/apps/chat/page.tsx
+++ app/dashboard/(defaults)/apps/chat/page.tsx
@@ -1,15 +1,11 @@
 'use client';
 import ComponentsAppsChat from '@/components/apps/chat/components-apps-chat';
-import Cookies from 'js-cookie';
 import React from 'react';
 
 const Chat = () => {
-  // Retrieve current_user from cookies
-  const currentUser = Cookies.get('current_user');
-  // Parse the JSON string or set default if not found
-  const userId = currentUser ? JSON.parse(currentUser).uid : '1';
+ 
 
-  return <ComponentsAppsChat loginUserId={userId} />;
+  return <ComponentsAppsChat />;
 };
 
 export default Chat;
