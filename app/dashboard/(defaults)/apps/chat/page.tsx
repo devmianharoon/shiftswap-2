d35d1ee4e1f07e84fe0e3af1bd3d6efb46f50d8f
Syed-Khizar-k <khizarsyed4294@gmail.com>
2025-06-18 15:20:41 +0500

    middle ware

--- app/dashboard/(defaults)/apps/chat/page.tsx
+++ app/dashboard/(defaults)/apps/chat/page.tsx
@@ -4,12 +4,12 @@ import Cookies from 'js-cookie';
 import React from 'react';
 
 const Chat = () => {
-  // Retrieve current_user from cookies
-  const currentUser = Cookies.get('current_user');
-  // Parse the JSON string or set default if not found
-  const userId = currentUser ? JSON.parse(currentUser).uid : '1';
+    // Retrieve current_user from cookies
+    const currentUser = Cookies.get('current_user');
+    // Parse the JSON string or set default if not found
+    const userId = currentUser ? JSON.parse(currentUser).uid : '21`1';
 
-  return <ComponentsAppsChat loginUserId={userId} />;
+    return <ComponentsAppsChat loginUserId={userId} />;
 };
 
 export default Chat;
